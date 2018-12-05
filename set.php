6eb4ef938f92f389cbc09c95c330c9a1bac3a47e
SteggyH <steggy@gmail.com>
2018-12-05 15:22:10 -0800

    change values to allow connection to mnsl server at house - change per environment

---
+++ set.php
@@ -0,0 +1,34 @@
+<?php
+
+//Change value below to your company name
+//CompanyName = "Litrex Corporation"
+$companyname = 'MNSL';
+
+//Change value below to the logo file located in the images directory
+$CompanyLogo = "logosm.gif";
+
+//MySQL info for database connection
+// No spaces between values and equal sign 
+$dbusername="mnsl";
+$dbpassword="score";
+$database="mnsl";
+$dbhost="192.168.33.45";
+
+// Display message on room display
+$displaymessage = 'no';
+
+$lsession = "29";
+
+//Just the domain name of the company NO @ signs
+$EmailDomain = "mondaynightshootingleague.org";
+
+//Change to the persons email who administrating the system
+//This can be more than one email address seperated by semicolan
+$AdminEmail = "Admin@yourcompany.com";
+
+//Change to the address used by the request system
+$ReqEmail = "request@yourcompany.com";
+
+
+?>
+
